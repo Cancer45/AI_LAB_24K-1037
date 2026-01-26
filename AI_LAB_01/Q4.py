532995be96d8a86f53c9b5cdbb5959c6ec151976
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2026-01-26 07:39:36 +0500

    Update Q4.py

--- AI_LAB_01/Q4.py
+++ AI_LAB_01/Q4.py
@@ -1,5 +1,3 @@
-import sys
-
 print("1. Add two numbers\n2. Subtract two numbers\n3. Exit")
 
 while True:
@@ -8,16 +6,14 @@ while True:
     if choice > 0 and choice < 4:
         break
 
-# used sys.exit() since no native python alternative for breaking execution
-if choice != 1 and choice != 2:
+if choice == 1 and choice == 2:
+    n = int(input("first number: "))
+    m = int(input("second number: "))
+
+    if choice == 1:
+        print(n + m)
+    elif choice == 2:
+        print(n - m)
+
+else:
     print("exiting...")
-    sys.exit()
-
-n = int(input("first number: "))
-m = int(input("second number: "))
-
-if choice == 1:
-    print(n + m)
-elif choice == 2:
-    print(n - m)
-
